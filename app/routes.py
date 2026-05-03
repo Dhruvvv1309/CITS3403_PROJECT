efff80cf0564c37d7519e8115b8ba7eedf3b0938
hadiakh101 <24761386@student.uwa.edu.au>
2026-05-03 17:00:40 +0800

    add database setup and coffee log model

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import flash, redirect, render_template, url_for
-from app import app
+from app import app, db
 from app.forms import CoffeeLogForm
+import os
+
+from app.models import CoffeeLog
 
 @app.route('/')
 def home(): #main page is the login page
@@ -20,11 +23,26 @@ def my_journal():
 
 @app.route('/log-coffee', methods=['GET', 'POST'])
 def log_coffee():
-    form=CoffeeLogForm()
+    form = CoffeeLogForm()
     if form.validate_on_submit():
-        #save to database once connected
+        #handle photo upload
+        photo = form.photo.data
+        photo_filename = photo.filename
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photo_filename))
+        photo_path = f'uploads/{photo_filename}'
+
+        #saving user input to database
+        entry = CoffeeLog(
+            cafe_name=form.cafe_name.data,
+            coffee_type=form.coffee_type.data,
+            rating=form.rating.data,
+            photo_path=photo_path,
+            notes=form.notes.data)
+        db.session.add(entry)
+        db.session.commit()
+
         flash('Coffee logged successfully!')
-        return redirect(url_for('my_journal'))
-    return render_template('log-coffee.html', title='Log a Coffee', form=form)
+        return redirect(url_for('my_journal')) #go to the journal page after coffee logged
+    return render_template('log-coffee.html', title='Log a Coffee', form=form) #render the log-coffee page with the form for user input
         
         
